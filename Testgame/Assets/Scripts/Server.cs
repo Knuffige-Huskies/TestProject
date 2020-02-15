b5ed2f25b91cb17f147345fb297c7e6640882a08
René Schwetschenau <schwetschenau.rene@yahoo.de>
2020-02-15 23:33:07 +0100

    Added 2 Lists to Server

--- Testgame/Assets/Scripts/Server.cs
+++ Testgame/Assets/Scripts/Server.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Networking.Transport;
 using UnityEngine;
 
 public class Server : MonoBehaviour
 {
+
+    private Dictionary<int, NetworkAdapter> m_Clients;
+    private Dictionary<int, GameObject> m_GameObjects;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +19,6 @@ public class Server : MonoBehaviour
     {
         
     }
+
+
 }
